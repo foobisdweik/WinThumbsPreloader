dc5da9ac023920a77c1c65a80cd37b26e7fd3065
foobisdweik <100015214+foobisdweik@users.noreply.github.com>
2025-12-25 22:52:49 -0800

    Optimize thumbnail preloading with streaming scanner
    Refactor thumbnail preloading to use streaming scanner and simplify processing logic.

--- WinThumbsPreloader/WinThumbsPreloader/ThumbnailsPreloader.cs
+++ WinThumbsPreloader/WinThumbsPreloader/ThumbnailsPreloader.cs
@@ -1,237 +1,64 @@
-using System;
-using System.Windows.Forms;
-using System.IO;
-using WinThumbsPreloader.Properties;
-using System.Threading.Tasks;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading;
-using System.Runtime.InteropServices;
-
-namespace WinThumbsPreloader
+private async void Run()
 {
-    public enum ThumbnailsPreloaderState
+    await Task.Run(() =>
     {
-        New,
-        GettingNumberOfItems,
-        Processing,
-        Canceled,
-        Done
-    }
+        using (Process p = Process.GetCurrentProcess())
+            p.PriorityClass = ProcessPriorityClass.BelowNormal;
 
-    //Preload all thumbnails, show progress dialog
-    class ThumbnailsPreloader
-    {
-        private DirectoryScanner directoryScanner;
-        private ProgressDialog progressDialog;
-        private System.Windows.Forms.Timer progressDialogUpdateTimer;
+        state = ThumbnailsPreloaderState.Processing; // Skip "GettingNumberOfItems" phase entirely
+        
+        // Use the streaming scanner
+        var fileStream = directoryScanner.GetFiles();
 
-        protected bool _multiThreaded;
-
-        public ThumbnailsPreloaderState state = ThumbnailsPreloaderState.GettingNumberOfItems;
-        public ThumbnailsPreloaderState prevState = ThumbnailsPreloaderState.New;
-        public int totalItemsCount = 0;
-        public int processedItemsCount = 0;
-        public string currentFile = "";
-
-        string executablePath = "";
-
-        // Definitions needed for instantiation
-        Guid CLSID_LocalThumbnailCache = new Guid("50ef4544-ac9f-4a8e-b21b-8a26180db13f");
-        Guid IID_IShellItem = new Guid("43826d1e-e718-42ee-bc55-a1e261c37bfe");
-
-        public ThumbnailsPreloader(string path, bool includeNestedDirectories, bool silentMode, bool multiThreaded)
+        if (!_multiThreaded)
         {
-            //Set the process priority to Below Normal to prevent system unresponsiveness
-            using (Process p = Process.GetCurrentProcess())
-                p.PriorityClass = ProcessPriorityClass.BelowNormal;
-
-            // Single File Mode for when passing a file through the command line or preloading a .svg file
-            executablePath = Process.GetCurrentProcess().MainModule.FileName;
-            FileAttributes fAt = File.GetAttributes(path);
-            if (!fAt.HasFlag(FileAttributes.Directory)) // path is file and not a directory
+            var TBCacheType = Type.GetTypeFromCLSID(CLSID_LocalThumbnailCache);
+            var tbCache = (ThumbnailPreloader.IThumbnailCache)Activator.CreateInstance(TBCacheType);
+            try
             {
-                // Instantiate cache just for this single file
-                var TBCacheType = Type.GetTypeFromCLSID(CLSID_LocalThumbnailCache);
-                var tbCache = (ThumbnailPreloader.IThumbnailCache)Activator.CreateInstance(TBCacheType);
-                try 
+                foreach (string item in fileStream)
                 {
-                    ThumbnailPreloader.PreloadThumbnail(path, tbCache, IID_IShellItem); 
-                }
-                finally
-                {
-                    if (tbCache != null) Marshal.ReleaseComObject(tbCache);
-                }
-                Environment.Exit(0);
-            }
-
-            // Normal mode
-            directoryScanner = new DirectoryScanner(path, includeNestedDirectories);
-            if (!silentMode)
-            {
-                InitProgressDialog();
-                InitProgressDialogUpdateTimer();
-            }
-            _multiThreaded = multiThreaded;
-            Run();
-        }
-
-        private void InitProgressDialog()
-        {
-            progressDialog = new ProgressDialog();
-            progressDialog.AutoClose = false;
-            progressDialog.ShowTimeRemaining = false;
-            progressDialog.Title = "WinThumbsPreloader";
-            progressDialog.CancelMessage = Resources.ThumbnailsPreloader_CancelMessage;
-            progressDialog.Maximum = 100;
-            progressDialog.Value = 0;
-            progressDialog.Show();
-            UpdateProgressDialog(null, null);
-        }
-
-        private void InitProgressDialogUpdateTimer()
-        {
-            progressDialogUpdateTimer = new System.Windows.Forms.Timer();
-            progressDialogUpdateTimer.Interval = 250;
-            progressDialogUpdateTimer.Tick += new EventHandler(UpdateProgressDialog);
-            progressDialogUpdateTimer.Start();
-        }
-
-        private void UpdateProgressDialog(object sender, EventArgs e)
-        {
-            if (progressDialog.HasUserCancelled)
-            {
-                state = ThumbnailsPreloaderState.Canceled;
-            }
-            else if (state == ThumbnailsPreloaderState.GettingNumberOfItems)
-            {
-                if (prevState != state)
-                {
-                    prevState = state;
-                    progressDialog.Line1 = Resources.ThumbnailsPreloader_PreloadingThumbnails;
-                    progressDialog.Line3 = Resources.ThumbnailsPreloader_CalculatingNumberOfItems;
-                    progressDialog.Marquee = true;
-                }
-                progressDialog.Line2 = String.Format(Resources.ThumbnailsPreloader_Discovered0Items, totalItemsCount);
-            }
-            else if (state == ThumbnailsPreloaderState.Processing)
-            {
-                if (prevState != state)
-                {
-                    prevState = state;
-                    progressDialog.Line1 = String.Format(Resources.ThumbnailsPreloader_PreloadingThumbnailsFor0Items, totalItemsCount);
-                    progressDialog.Maximum = totalItemsCount;
-                    progressDialog.Marquee = false;
-                }
-                // Calculate percentage safely
-                int currentCount = processedItemsCount; 
-                int percent = totalItemsCount > 0 ? (currentCount * 100) / totalItemsCount : 0;
-                
-                progressDialog.Title = String.Format(Resources.ThumbnailsPreloader_Processing, percent);
-                progressDialog.Line2 = Resources.ThumbnailsPreloader_Name + ": " + Path.GetFileName(currentFile);
-                progressDialog.Line3 = String.Format(Resources.ThumbnailsPreloader_ItemsRemaining, totalItemsCount - currentCount);
-                progressDialog.Value = currentCount;
-            }
-        }
-
-        private async void Run()
-        {
-            await Task.Run(() =>
-            {
-                using (Process p = Process.GetCurrentProcess())
-                    p.PriorityClass = ProcessPriorityClass.BelowNormal;
-
-                state = ThumbnailsPreloaderState.GettingNumberOfItems;
-
-                List<string> items = new List<string>();
-                foreach (Tuple<int, List<string>> itemsCount in directoryScanner.GetItemsCount())
-                {
-                    totalItemsCount = itemsCount.Item1;
-                    items = itemsCount.Item2;
-                    if (state == ThumbnailsPreloaderState.Canceled) return;
-                }
-                if (totalItemsCount == 0)
-                {
-                    state = ThumbnailsPreloaderState.Done;
-                    return;
-                }
-
-                state = ThumbnailsPreloaderState.Processing;
-
-                if (!_multiThreaded)
-                {
-                    // Single-threaded optimization: Create Cache ONCE
-                    var TBCacheType = Type.GetTypeFromCLSID(CLSID_LocalThumbnailCache);
-                    var tbCache = (ThumbnailPreloader.IThumbnailCache)Activator.CreateInstance(TBCacheType);
+                    if (state == ThumbnailsPreloaderState.Canceled) break;
+                    
+                    // Update total count dynamically or just show "Processed: X"
+                    // Since we are streaming, we don't know the Total ahead of time.
+                    // If you strictly need a Progress Bar (0-100%), you MUST do the two-pass scan (scan all, then process).
+                    // If you prefer speed, you switch the UI to "Processed: 500 items..." (Indeterminate progress).
                     
                     try
                     {
-                        foreach (string item in items)
-                        {
-                            try
-                            {
-                                currentFile = item;
-                                ThumbnailPreloader.PreloadThumbnail(item, tbCache, IID_IShellItem);
-                                
-                                Interlocked.Increment(ref processedItemsCount);
-                                
-                                if (processedItemsCount == totalItemsCount) state = ThumbnailsPreloaderState.Done;
-                                if (state == ThumbnailsPreloaderState.Canceled) 
-                                {
-                                    Application.Exit();
-                                    return; 
-                                }
-                            }
-                            catch (Exception) { } 
-                        }
+                        currentFile = item;
+                        ThumbnailPreloader.PreloadThumbnail(item, tbCache, IID_IShellItem);
+                        Interlocked.Increment(ref processedItemsCount);
                     }
-                    finally
-                    {
-                        if (tbCache != null) Marshal.ReleaseComObject(tbCache);
-                    }
+                    catch { }
                 }
-                else
-                {
-                    // Multi-threaded optimization: Create Cache ONCE PER THREAD
-                    Parallel.ForEach(
-                        items,
-                        new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount },
-                        // 1. Init: Create the COM instance for this thread
-                        () => 
-                        {
-                            var TBCacheType = Type.GetTypeFromCLSID(CLSID_LocalThumbnailCache);
-                            return (ThumbnailPreloader.IThumbnailCache)Activator.CreateInstance(TBCacheType);
-                        },
-                        // 2. Body: Process file using the thread-local cache
-                        (item, loopState, index, tbCache) =>
-                        {
-                            try
-                            {
-                                currentFile = item; // Still racey for UI, but acceptable for display only
-                                ThumbnailPreloader.PreloadThumbnail(item, tbCache, IID_IShellItem);
-                                
-                                Interlocked.Increment(ref processedItemsCount);
-                                
-                                if (processedItemsCount == totalItemsCount) state = ThumbnailsPreloaderState.Done;
-                                if (state == ThumbnailsPreloaderState.Canceled) 
-                                {
-                                    loopState.Stop();
-                                    Application.Exit();
-                                }
-                            }
-                            catch (Exception) { } 
-                            return tbCache; // Pass cache to next iteration
-                        },
-                        // 3. Finally: Cleanup COM instance for this thread
-                        (tbCache) => 
-                        {
-                             if (tbCache != null) Marshal.ReleaseComObject(tbCache);
-                        }
-                    );
-                }
-            });
-            Application.Exit();
+            }
+            finally 
+            {
+                if (tbCache != null) Marshal.ReleaseComObject(tbCache);
+            }
         }
-    }
+        else
+        {
+             // For Parallel.ForEach to work with a stream, it simply consumes the IEnumerable
+             Parallel.ForEach(fileStream, 
+                new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount },
+                () => {
+                    var TBCacheType = Type.GetTypeFromCLSID(CLSID_LocalThumbnailCache);
+                    return (ThumbnailPreloader.IThumbnailCache)Activator.CreateInstance(TBCacheType);
+                },
+                (item, loopState, index, tbCache) => {
+                    if (state == ThumbnailsPreloaderState.Canceled) loopState.Stop();
+                    ThumbnailPreloader.PreloadThumbnail(item, tbCache, IID_IShellItem);
+                    Interlocked.Increment(ref processedItemsCount);
+                    return tbCache;
+                },
+                (tbCache) => { if (tbCache != null) Marshal.ReleaseComObject(tbCache); }
+             );
+        }
+        
+        state = ThumbnailsPreloaderState.Done;
+    });
+    Application.Exit();
 }
